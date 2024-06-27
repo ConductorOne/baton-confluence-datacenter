362befbb3939d1b2a2b66c540d8f7ed992de3768
Marcos Gaeta <mgaeta89@gmail.com>
2024-06-27 15:49:21 -0700

    Add provisioning for space permissions

--- pkg/connector/client/models.go
+++ pkg/connector/client/models.go
@@ -25,3 +25,38 @@ type confluenceGroupList struct {
 	Size    int
 	Results []ConfluenceGroup
 }
+
+type ConfluenceSpaceDescriptionValue struct {
+	Value          string `json:"value"`
+	Representation string `json:"representation"`
+}
+
+type ConfluenceSpaceDescription struct {
+	Plain ConfluenceSpaceDescriptionValue `json:"plain"`
+}
+
+type ConfluenceSpace struct {
+	Id          int                        `json:"id"`
+	Description ConfluenceSpaceDescription `json:"description"`
+	Key         string                     `json:"key"`
+	Name        string                     `json:"name"`
+	Type        string                     `json:"type"`
+}
+
+type confluenceSpaceList struct {
+	Start   int
+	Limit   int
+	Size    int
+	Results []ConfluenceSpace
+}
+
+type ConfluenceSpacePermission struct {
+	GroupName string `json:"groupName"`
+	Type      string `json:"type"` // duplicate of the `Type` from above.
+	UserName  string `json:"userName"`
+}
+
+type ConfluenceSpacePermissionList struct {
+	SpacePermissions []ConfluenceSpacePermission `json:"spacePermissions"`
+	Type             string                      `json:"type"`
+}
