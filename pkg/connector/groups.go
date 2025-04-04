84fd4ab9461d1ea3d775c53ed9e89353c0ba5478
sergio.corral <sergio.corral@conductorone.com>
2025-04-04 17:22:13 -0300

    feat: Support groups with slashes in names (CONFCLOUD-68869)

--- pkg/connector/groups.go
+++ pkg/connector/groups.go
@@ -3,6 +3,8 @@ package connector
 import (
 	"context"
 	"fmt"
+	"strings"
+	"sync"
 
 	"github.com/conductorone/baton-confluence-datacenter/pkg/connector/client"
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
@@ -19,8 +21,21 @@ const (
 	groupMemberEntitlement = "member"
 )
 
+var (
+	groupMembersCacheOnce         sync.Once
+	groupMembersCacheInstance     *groupMembersCache
+	enableSupportSlashInGroupName bool
+)
+
+type groupMembersCache struct {
+	cache  map[string][]client.ConfluenceUser
+	client client.ConfluenceClient
+	mutex  sync.RWMutex
+}
+
 type groupBuilder struct {
 	client client.ConfluenceClient
+	cache  *groupMembersCache
 }
 
 func (o *groupBuilder) ResourceType(_ context.Context) *v2.ResourceType {
@@ -39,13 +54,32 @@ func (o *groupBuilder) List(
 	annotations.Annotations,
 	error,
 ) {
+	logger := ctxzap.Extract(ctx)
+	// Reset the cache if we're starting a new sync.
+	// This cache is a workaround for a Confluence API limitation. See: https://jira.atlassian.com/browse/CONFCLOUD-68869
+	if pToken.Token == "" {
+		ResetGroupMembersCache()
+		enableSupportSlashInGroupName = false
+	}
+
 	groups, nextToken, ratelimitData, err := o.client.GetGroups(ctx, pToken.Token)
 	outputAnnotations := WithRateLimitAnnotations(ratelimitData)
 	if err != nil {
 		return nil, "", outputAnnotations, err
 	}
+
 	rv := make([]*v2.Resource, 0)
 	for _, group := range groups {
+		// If the group name contains a slash and support slash in group name is not enabled, enable it
+		// This is a workaround for a Confluence API limitation. See: https://jira.atlassian.com/browse/CONFCLOUD-68869
+		if !enableSupportSlashInGroupName && strings.Contains(group.Name, "/") {
+			logger.Info(
+				"baton-confluence-datacenter: support slash in group name is enabled, this is a workaround for a Confluence API limitation. See: https://jira.atlassian.com/browse/CONFCLOUD-68869",
+				zap.String("group_name", group.Name),
+			)
+			enableSupportSlashInGroupName = true
+		}
+
 		groupCopy := group
 		ur, err := groupResource(ctx, &groupCopy)
 		if err != nil {
@@ -106,25 +140,56 @@ func (o *groupBuilder) Grants(
 	annotations.Annotations,
 	error,
 ) {
-	bag := &pagination.Bag{}
-	err := bag.Unmarshal(pToken.Token)
-	if err != nil {
-		return nil, "", nil, err
-	}
-	if bag.Current() == nil {
-		bag.Push(pagination.PageState{
-			ResourceTypeID: groupResourceType.Id,
-		})
-	}
+	logger := ctxzap.Extract(ctx)
+	nextPage := ""
+	outputAnnotations := WithRateLimitAnnotations(nil)
+	var users []client.ConfluenceUser
 
-	users, token, ratelimitData, err := o.client.GetGroupMembers(
-		ctx,
-		bag.PageToken(),
-		resource.DisplayName,
-	)
-	outputAnnotations := WithRateLimitAnnotations(ratelimitData)
-	if err != nil {
-		return nil, "", outputAnnotations, err
+	// When we've detected groups containing forward slashes ('/') in the system,
+	// we cannot use the direct group members API endpoint for any group lookups
+	// due to a Confluence API limitation (see: https://jira.atlassian.com/browse/CONFCLOUD-68869).
+	// Instead, we use a cached approach that:
+	// 1. Lists all users in the system
+	// 2. For each user, fetches their group memberships
+	// 3. Builds a reverse mapping of group -> members
+	if enableSupportSlashInGroupName {
+		logger.Debug("baton-confluence-datacenter: using cached group members list due to Confluence API limitation")
+		groupMembers, err := getGroupMembersCache(o.client).GetGroupMembersList(ctx)
+		if err != nil {
+			return nil, "", nil, err
+		}
+		users = groupMembers[resource.DisplayName]
+	} else {
+		logger.Debug("baton-confluence-datacenter: using standard API endpoint for group members list due to Confluence API limitation")
+		// If no groups with slashes have been detected in the system, we use the standard API endpoint
+		// to directly fetch members which is more efficient for individual group lookups
+		token := ""
+		bag := &pagination.Bag{}
+		err := bag.Unmarshal(pToken.Token)
+		if err != nil {
+			return nil, "", nil, err
+		}
+		if bag.Current() == nil {
+			bag.Push(pagination.PageState{
+				ResourceTypeID: groupResourceType.Id,
+			})
+		}
+
+		var ratelimitData *v2.RateLimitDescription
+		users, token, ratelimitData, err = o.client.GetGroupMembers(
+			ctx,
+			bag.PageToken(),
+			resource.DisplayName,
+		)
+		outputAnnotations = WithRateLimitAnnotations(ratelimitData)
+		if err != nil {
+			return nil, "", outputAnnotations, err
+		}
+
+		nextPage, err = bag.NextToken(token)
+		if err != nil {
+			return nil, "", nil, err
+		}
 	}
 
 	var groups []*v2.Grant
@@ -139,10 +204,6 @@ func (o *groupBuilder) Grants(
 		))
 	}
 
-	nextPage, err := bag.NextToken(token)
-	if err != nil {
-		return nil, "", nil, err
-	}
 	return groups, nextPage, outputAnnotations, nil
 }
 
@@ -162,9 +223,33 @@ func (o *groupBuilder) Grant(
 		return nil, fmt.Errorf("baton-confluence-datacenter: only users can be granted group membership")
 	}
 
+	// Check for slash in group name. This is a workaround for a Confluence API limitation. See: https://jira.atlassian.com/browse/CONFCLOUD-68869
+	if strings.Contains(entitlement.Resource.Id.Resource, "/") {
+		logger.Warn(
+			"baton-confluence-datacenter: cannot grant membership to groups containing '/' in their name",
+			zap.String("group_name", entitlement.Resource.Id.Resource),
+		)
+		return nil, fmt.Errorf(
+			"baton-confluence-datacenter: groups containing '/' in their name are not supported for grant operations " +
+				"due to Confluence API limitations. See: https://jira.atlassian.com/browse/CONFCLOUD-68869",
+		)
+	}
+
 	userDetail, err := o.client.GetUserByKey(ctx, principal.Id.Resource)
 	if err != nil {
 		return nil, err
+	}
+
+	// Check for slash in username. This is a workaround for a Confluence API limitation. See: https://jira.atlassian.com/browse/CONFCLOUD-68869
+	if strings.Contains(userDetail.Username, "/") {
+		logger.Warn(
+			"baton-confluence-datacenter: cannot grant membership to users with '/' in their username",
+			zap.String("username", userDetail.Username),
+		)
+		return nil, fmt.Errorf(
+			"baton-confluence-datacenter: users with '/' in their username are not supported for grant operations " +
+				"due to Confluence API limitations. See: https://jira.atlassian.com/browse/CONFCLOUD-68869",
+		)
 	}
 
 	ratelimitData, err := o.client.AddGroupMember(
@@ -194,9 +279,33 @@ func (o *groupBuilder) Revoke(
 		return nil, fmt.Errorf("baton-confluence-datacenter: only users can have group membership revoked")
 	}
 
+	// Check for slash in group name. This is a workaround for a Confluence API limitation. See: https://jira.atlassian.com/browse/CONFCLOUD-68869
+	if strings.Contains(ent.Resource.Id.Resource, "/") {
+		logger.Warn(
+			"baton-confluence-datacenter: cannot revoke membership from groups containing '/' in their name",
+			zap.String("group_name", ent.Resource.Id.Resource),
+		)
+		return nil, fmt.Errorf(
+			"baton-confluence-datacenter: groups with '/' in their name are not supported for revoke operations " +
+				"due to Confluence API limitations. See: https://jira.atlassian.com/browse/CONFCLOUD-68869",
+		)
+	}
+
 	userDetail, err := o.client.GetUserByKey(ctx, principal.Id.Resource)
 	if err != nil {
 		return nil, err
+	}
+
+	// Check for slash in username. This is a workaround for a Confluence API limitation. See: https://jira.atlassian.com/browse/CONFCLOUD-68869
+	if strings.Contains(userDetail.Username, "/") {
+		logger.Warn(
+			"baton-confluence-datacenter: cannot revoke membership from users with '/' in their username",
+			zap.String("username", userDetail.Username),
+		)
+		return nil, fmt.Errorf(
+			"baton-confluence-datacenter: users with '/' in their username are not supported for revoke operations " +
+				"due to Confluence API limitations. See: https://jira.atlassian.com/browse/CONFCLOUD-68869",
+		)
 	}
 
 	ratelimitData, err := o.client.RemoveGroupMember(
@@ -208,9 +317,10 @@ func (o *groupBuilder) Revoke(
 	return outputAnnotations, err
 }
 
-func newGroupBuilder(client client.ConfluenceClient) *groupBuilder {
+func newGroupBuilder(cclient client.ConfluenceClient) *groupBuilder {
 	return &groupBuilder{
-		client: client,
+		client: cclient,
+		cache:  getGroupMembersCache(cclient),
 	}
 }
 
@@ -233,4 +343,94 @@ func groupResource(_ context.Context, group *client.ConfluenceGroup) (*v2.Resour
 	}
 
 	return createdResource, nil
+}
+
+// ******************************************
+// Group Members Cache
+// This cache is implemented as a workaround for a Confluence API limitation
+// where the direct group members endpoint does not support group names containing
+// forward slashes ('/'). Instead, we build the membership list by querying all
+// users and their group memberships.
+// ******************************************
+
+// getGroupMembersCache returns a singleton instance of the groupMembersCache.
+func getGroupMembersCache(cclient client.ConfluenceClient) *groupMembersCache {
+	groupMembersCacheOnce.Do(func() {
+		groupMembersCacheInstance = &groupMembersCache{
+			cache:  make(map[string][]client.ConfluenceUser),
+			client: cclient,
+			mutex:  sync.RWMutex{},
+		}
+	})
+	return groupMembersCacheInstance
+}
+
+// ResetGroupMembersCache replaces the Once instance with a new one.
+func ResetGroupMembersCache() {
+	groupMembersCacheInstance = nil
+	groupMembersCacheOnce = sync.Once{}
+}
+
+// GetGroupMembersList uses the cache.
+func (c *groupMembersCache) GetGroupMembersList(ctx context.Context) (map[string][]client.ConfluenceUser, error) {
+	c.mutex.RLock()
+	if len(c.cache) > 0 {
+		defer c.mutex.RUnlock()
+		return c.cache, nil
+	}
+	c.mutex.RUnlock()
+
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	// Double-check after acquiring write lock
+	if len(c.cache) > 0 {
+		return c.cache, nil
+	}
+
+	// Existing GetGroupMembersList logic here, but store result in c.cache
+	groupMembers := make(map[string][]client.ConfluenceUser)
+	userPageToken := ""
+	// Paginate through all users
+	for {
+		users, userNextToken, _, err := c.client.GetUsers(ctx, userPageToken)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get users: %w", err)
+		}
+
+		// Process users in current page
+		for _, user := range users {
+			groupPageToken := ""
+			// Paginate through all groups for the user is member of
+			for {
+				groups, groupNextToken, _, err := c.client.GetGroupsByUserKey(ctx, groupPageToken, user.UserKey)
+				if err != nil {
+					return nil, fmt.Errorf("failed to get groups for user %s: %w", user.Username, err)
+				}
+
+				// Add user to each group they belong to
+				for _, group := range groups {
+					if _, exists := groupMembers[group.Name]; !exists {
+						groupMembers[group.Name] = make([]client.ConfluenceUser, 0)
+					}
+					groupMembers[group.Name] = append(groupMembers[group.Name], user)
+				}
+
+				// Break if no more pages
+				if groupNextToken == "" {
+					break
+				}
+				groupPageToken = groupNextToken
+			}
+		}
+
+		// Break if no more pages
+		if userNextToken == "" {
+			break
+		}
+		userPageToken = userNextToken
+	}
+
+	c.cache = groupMembers
+	return groupMembers, nil
 }
